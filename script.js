f1e9b93082f40006e722047c3b1c43d88b908903
BlueBomber621 <126737954+BlueBomber621@users.noreply.github.com>
2024-10-07 14:15:13 -0600

    Update script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const Counter = document.querySelector("#clicks");
+const Button = document.querySelector(".button");
+let interval;
+let Number = 0;
+
+function btnPressed() {
+  Number++;
+  Counter.textContent = Number;
+}
+
+Button.addEventListener("mousedown", () => {
+  interval = setInterval(() => {
+    // Button Pressed and Held
+    btnPressed();
+  }, 100);
+});
+
+Button.addEventListener("mouseup", () => {
+  clearInterval(interval);
+});
